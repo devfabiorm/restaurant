eb53d2459e705aa22581928c8980fa6fec0fcd68
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-08-10 20:53:24 -0300

    Created remove reservation action and called on function in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import { RootState } from "./app/store";
+import ReservationsCard from "./components/ReservationCard";
+import { addReservation } from "./features/reservationsSlice";
 
 function App() {
+
+  const [reservationNameInput, setReservationNameInput] = useState(""); 
+  const reservations = useSelector((state: RootState) => state.reservations.value);
+
+  const dispatch = useDispatch();
+
+  const handleAddReservations = () => {
+    if(!reservationNameInput) return;
+
+    dispatch(addReservation(reservationNameInput));
+    setReservationNameInput("")
+  }
+
   return (
     <div className="App">
       <div className="container">
@@ -9,12 +26,16 @@ function App() {
           <div>
             <h5 className="reservation-header">Reservations</h5>
             <div className="reservation-cards-container">
-              <div className="reservation-card-container">Laith Harb</div>
+              { reservations.map((name, index) => {
+                return <ReservationsCard name={name} index={index} />
+              })}
             </div>
           </div>
           <div className="reservation-input-container">
-            <input />
-            <button>Add</button>
+            <input
+              value={reservationNameInput}
+              onChange={(e) => setReservationNameInput(e.target.value)} />
+            <button onClick={handleAddReservations}>Add</button>
           </div>
         </div>
         <div className="customer-food-container">
